51454c12d74d6577a080a23b937b12e3fcec366d
Sthefanny <s.gonzaga@cinq.com.br>
2018-05-24 18:52:40 -0300

    Location is working

--- TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
@@ -1,20 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
+using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+using System;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
+using Position = Xamarin.Forms.Maps.Position;
 
 namespace TravelRecordApp
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MapPage : ContentPage
-	{
-		public MapPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var locator = CrossGeolocator.Current;
+            locator.PositionChanged += Locator_PositionChanged;
+            await locator.StartListeningAsync(TimeSpan.Zero, 100);
+
+            var position = await locator.GetPositionAsync();
+            SetMapPosition(position);
+        }
+
+        private void Locator_PositionChanged(object sender, PositionEventArgs positionEventArgs)
+        {
+            SetMapPosition(positionEventArgs.Position);
+        }
+
+        private void SetMapPosition(Plugin.Geolocator.Abstractions.Position position)
+        {
+            var center = new Position(position.Latitude, position.Longitude);
+            var span = new MapSpan(center, 2, 2);
+            locationsMap.MoveToRegion(span);
+        }
+    }
 }
