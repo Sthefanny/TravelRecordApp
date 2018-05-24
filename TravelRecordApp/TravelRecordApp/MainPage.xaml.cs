7569c14122a676763b9a57ba7cef65f26c3e00e1
Sthefanny <s.gonzaga@cinq.com.br>
2018-05-24 13:20:52 -0300

    UI already done

---
+++ TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            var isEmailEmpty = string.IsNullOrWhiteSpace(EmailEntry.Text);
+            var isPasswordEmpty = string.IsNullOrWhiteSpace(PasswordEntry.Text);
+
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
