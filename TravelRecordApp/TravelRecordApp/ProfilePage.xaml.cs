7946ca921371d2f546d22f5146fad2cc878260aa
Sthefanny <s.gonzaga@cinq.com.br>
2018-05-25 17:17:44 -0300

    Profile page is ready

--- TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
@@ -1,20 +1,45 @@
-﻿using System;
+﻿
+using SQLite;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using TravelRecordApp.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace TravelRecordApp
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ProfilePage : ContentPage
-	{
-		public ProfilePage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using (var conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                var postTable = conn.Table<Post>().Where(x => x.CategoryId != null).ToList();
+
+                var categories = postTable.OrderBy(p => p.CategoryId).Select(p => p.CategoryName).Distinct().ToList();
+
+                var categoriesCount = new Dictionary<string, int>();
+
+                foreach (var category in categories)
+                {
+                    var count = postTable.Where(p => p.CategoryName == category).ToList().Count;
+
+                    if (!string.IsNullOrWhiteSpace(category))
+                        categoriesCount.Add(category, count);
+                }
+
+                categoriesListView.ItemsSource = categoriesCount;
+
+                postCountLabel.Text = postTable.Count.ToString();
+            }
+        }
+    }
 }
