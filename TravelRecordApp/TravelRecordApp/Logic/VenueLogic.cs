85904094833f2d52a799a92e40798b4361ee7a50
Sthefanny <s.gonzaga@cinq.com.br>
2018-05-25 12:01:29 -0300

    Lista de endereços funcionando

--- TravelRecordApp/TravelRecordApp/Logic/VenueLogic.cs
+++ TravelRecordApp/TravelRecordApp/Logic/VenueLogic.cs
@@ -1,4 +1,8 @@
-﻿using TravelRecordApp.Model;
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TravelRecordApp.Model;
 
 namespace TravelRecordApp.Logic
 {
@@ -10,15 +14,15 @@ namespace TravelRecordApp.Logic
 
             var url = VenueRoot.GenerateUrl(latitute, longitude);
 
-            //using (var client = new HttpClient())
-            //{
-            //    var response = await client.GetAsync(url);
-            //    var json = await response.Content.ReadAsStringAsync();
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                var json = await response.Content.ReadAsStringAsync();
 
-            //    var venueRoot = JsonConvert.DeserializeObject<VenueRoot>(json);
+                var venueRoot = JsonConvert.DeserializeObject<VenueRoot>(json);
 
-            //    venues = venueRoot.Response.Venues as List<Venue>;
-            //}
+                venues = venueRoot.Response.Venues as List<Venue>;
+            }
 
             return venues;
         }
